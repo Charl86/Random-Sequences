b7da4512eb1ef18e9f78ccf78912c84f9f3bd52f
Carlos Hernandez <carl.hernan34@gmail.com>
2020-04-27 01:28:28 -0400

    Changed all occurrences of 'deviación' for 'desviación'. Added my name and general information about the program at the top. Modified some comments.

--- Random Sequences.cpp
+++ Random Sequences.cpp
@@ -1,3 +1,13 @@
+/*
+    Carlos A. Hernández
+
+    Este programa se encarga de crear (o leer) un archivo de secuencias random. Cada
+    secuencia tendrá generalmente 10 números y la cantidad de secuencias es indicada
+    por el usuario. Luego de las secuencias random, se crea un nuevo archivo en donde
+    las secuencias van a ser normalizadas desplegadas, seguidas por la media y la desviación
+    estándar de la secuencia.
+*/
+
 #include <iostream>
 #include <iomanip>
 #include <fstream>
@@ -18,7 +28,7 @@ void makeSequences(fstream &, int, long double [][5][NUMS_POR_SEC]);  // Crea la
 void getSequences(fstream &, fstream &, int, long double [][5][NUMS_POR_SEC]);  // Normaliza las secuencias.
 
 // Nuevas funciones implementadas:
-double calcDeviation(long double [][5][NUMS_POR_SEC], int, double);  // Calcula la deviación estándar.
+double calcDeviation(long double [][5][NUMS_POR_SEC], int, double);  // Calcula la desviación estándar.
 string lowerCase(string);  // Devuelve el string provisto en lowercase.
 void readSequences(fstream &, int, long double [][5][NUMS_POR_SEC]);  // Lee las secuencias de un archivo existente.
 bool askReadFile();  // Pregunta si se desea leer un archivo existente o crear uno nuevo para las secuencias.
@@ -38,9 +48,9 @@ int main() {
     long double RandNums[numberOfSeqs][5][NUMS_POR_SEC];
     /* Se crea un arreglo de 3 dimensiones de la siguiente manera. Ejemplo:
         arreglo[] = {
-            {{rand(1), rand(2), ...}, {clicks}, {núm_mayor}, {media}, {deviación}},  // Secuencia 1
-            {{rand(7), rand(4), ...}, {clicks}, {núm_mayor}, {media}, {deviación}},  // Secuencia 2
-            {{rand(4), rand(10), ...}, {clicks}, {núm_mayor}, {media}, {deviación}},  // Secuencia 3
+            {{rand(1), rand(2), ...}, {clicks}, {núm_mayor}, {media}, {desviación}},  // Secuencia 1
+            {{rand(7), rand(4), ...}, {clicks}, {núm_mayor}, {media}, {desviación}},  // Secuencia 2
+            {{rand(4), rand(10), ...}, {clicks}, {núm_mayor}, {media}, {desviación}},  // Secuencia 3
             ...
         }
 
@@ -49,14 +59,14 @@ int main() {
         b) arreglo[2][0][n] denota el número n en la columna n, fila 2.
         c) arreglo[2][1][0] denota los clicks de la fila/secuencia 2.
         d) arreglo[2][2][0] denota el número mayor de la fila/secuencia 2.
-        e) arreglo[2][4][0] denota la deviación en la fila/secuencia 2 (dicha deviación va a ser definida
+        e) arreglo[2][4][0] denota la desviación en la fila/secuencia 2 (dicha desviación va a ser definida
                                                                         a la hora de normalizar la secuencia).
     */
 
     makeFilenames(SeqsFile, NormlicedFile, readFile);  // Crea los archivos.
 
-    if (readFile)  // Si readFile es cierto: si se va a leer un archivo
-        // Se leen los archivos.
+    if (readFile)  // Si readFile es cierto; es decir, si se va a leer un archivo
+        // se leen los archivos.
         readSequences(SeqsFile, numberOfSeqs, RandNums);
     else
         /* De lo contrario, crear un nuevo archivo y crear nuevas secuencias
@@ -97,12 +107,13 @@ void makeFilenames(fstream &Secuencias, fstream &Normalizadas, bool &readFile) {
         << " en donde desea guardar las secuencias:" << endl;
     }
     cin >> seqFilename;  // Se guarda el nombre del archivo para las secuencias sin normalizar.
+
     if (readFile) {  // Si se va a leer un archivo
 
-        // Abrir el archivo sin borrar su contenido previo.
+        // abrir el archivo sin borrar su contenido previo
         Secuencias.open(seqFilename + ".txt");
         
-        // Validar que el nombre provisto del archivo exista.
+        // validar que el nombre provisto del archivo exista.
         while (!Secuencias.is_open()) {  // Si no existe, dejarle saber al usuario que no existe
             cout << endl << "El archivo que ha ingresado no existe. Asegurese de que el archivo"
             << endl << "exista o que haya escrito su nombre correctamente." << endl;
@@ -113,7 +124,7 @@ void makeFilenames(fstream &Secuencias, fstream &Normalizadas, bool &readFile) {
         }
     }
     else  // Si no se va a leer un archivo
-        /* Crear un nuevo archivo con el nombre provisto si el mismo no existe,
+        /* crear un nuevo archivo con el nombre provisto si el mismo no existe,
         o borrar el contenido del archivo con el mismo nombre si el mismo ya existe. */
         Secuencias.open(seqFilename + ".txt", fstream::out | fstream::in | fstream::trunc);
 
@@ -138,15 +149,15 @@ bool askReadFile() {
         // Si la respuesta es remotamente afirmativa
         if (answer == "yes" || answer == "y"
             || answer == "si" || answer == "1")
-                return true;  // Devolver cierto.
+                return true;  // devolver ciertd.
         // Si la respuesta es un 'No' o algo similar
         else if (answer == "no" || answer == "n" || answer == "0")
-                return false;  // Devolver falso.
+                return false;  // devolver falso.
         // Si la respuesta ni es un "si" ni es un "no" (u otras alternativas)
         else
-            // Decirle al usuario que responda de una manera u otra.
+            // decirle al usuario que responda de una manera u otra.
             cout << endl << "Por favor, responda con un 'si' o un 'no'.";
-    } while (1);  /* Esto se va a repetir infinitamente, a menos que el usuario
+    } while (1);  /* Este proceso se va a repetir infinitamente, a menos que el usuario
                      responda correctamente. */
 }
 
@@ -154,7 +165,7 @@ void makeSequences(fstream &Secuencias, int num_sec, long double RandArray[][5][
     // Función para crear las secuencias de números random.
     
     struct timespec start, end;  // Variables para comenzar y terminar el 'cronómetro'.
-    double clicks;  // Variable para calcular los clicks.
+    double clicks;  // Variable para guardar los clicks.
 
     // Por cada secuencia hasta el número de secuencias provisto
     for (int i = 0; i < num_sec; i++) {
@@ -243,7 +254,7 @@ void getSequences(fstream &Secuencias, fstream &Normalizadas, int num_secs, long
     for (int i = 0; i < NUMS_POR_SEC; i++)
         Normalizadas << setw(11) <<"Value_" << (i + 1);  // header para los valores
 
-    // header para la media y la deviación:
+    // header para la media y la desviación:
     Normalizadas << setw(11) << "Mean" << setw(12) << "StdDev";
     // línea divisora:
     Normalizadas << endl << string(155, '-') << endl;
@@ -286,15 +297,15 @@ void getSequences(fstream &Secuencias, fstream &Normalizadas, int num_secs, long
         divididos por la cantidad de los mismos. */
         media = media/NUMS_POR_SEC;
 
-        // Se calcula la deviación dadas una secuencia n y su media.
+        // Se calcula la desviación dadas una secuencia n y su media.
         deviation = calcDeviation(RandArray, n, media);
 
-        /* Se guardan la media y la deviación en sus propios arreglos, después
+        /* Se guardan la media y la desviación en sus propios arreglos, después
         de los números normalizados de la secuencia n, sus clicks y el número mayor. */
         RandArray[n][3][0] = media;
         RandArray[n][4][0] = deviation;
 
-        // Se desplegan la media y la deviación en el archivo.
+        // Se desplegan la media y la desviación en el archivo.
         Normalizadas << setw(ESPACIO) << RandArray[n][3][0];
         Normalizadas << setw(ESPACIO) << RandArray[n][4][0];
         // Si no se itera por la última secuencia, insertar una nueva línea.
@@ -309,17 +320,17 @@ void getSequences(fstream &Secuencias, fstream &Normalizadas, int num_secs, long
 }
 
 double calcDeviation(long double RandArray[][5][NUMS_POR_SEC], int seq_idx, double seq_mean) {
-    // Función para calcular la deviación de la secuencia 'sec_idx' con la media 'seq_mean'.
+    // Función para calcular la desviación de la secuencia 'sec_idx' con la media 'seq_mean'.
 
-    double std_deviation = 0.0;  // Se inicializa la deviación.
+    double std_deviation = 0.0;  // Se inicializa la desviación.
 
     // Por cada número normalizado en la secuencia seq_idx
     for (int i = 0; i < NUMS_POR_SEC; i++) {
         /* se cuadra la diferencia entre el número i y la media de la secuencia, y se
-        añade a la deviación. */
+        añade a la desviación. */
         std_deviation += pow(RandArray[seq_idx][0][i] - seq_mean, 2);
     }
-    /* Finalmente, se divide la deviación por la cantidad de números por secuencia 
+    /* Finalmente, se divide la desviación por la cantidad de números por secuencia 
     y se le saca la raíz cuadrada */
     std_deviation = sqrt(std_deviation/NUMS_POR_SEC);
     return std_deviation;  // y se devuelve el resultado.
