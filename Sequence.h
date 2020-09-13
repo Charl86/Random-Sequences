5672c01678fb0c1b5a6d6098271f349106a949ca
Carlos Hernandez <carl.hernan34@gmail.com>
2020-09-12 21:56:14 -0400

    Translated comments to English.

--- Sequence.h
+++ Sequence.h
@@ -1,9 +1,4 @@
-﻿/*
-    Carlos A. Hernández
-    CCOM 3033
-*/
-
-#ifndef SEQUENCE_H
+﻿#ifndef SEQUENCE_H
 #define SEQUENCE_H
 
 #include <iostream>
@@ -13,118 +8,65 @@
 using namespace std;
 
 
-const int NUMS_POR_SEC = 10;  // La cantidad de números por secuencia.
+const int NUMS_POR_SEC = 10;  // Amount of numbers per sequence.
 
-struct Sequence {  // El struct llamado 'Secuencia'
-    std::string dataID;  // Miembro dataID, donde se va a guardar el número de la secuencia.
-    double mayor;  // El número mayor de la secuencia.
-    double media;  // La media de la secuencia.
-    double stdDev;  // La desviación estándar de la secuencia.
-    double clicks;  // Los clicks que se tomaron en producir la secuencia de números randoms.
-    double rand_numbers[NUMS_POR_SEC];  // Los números randoms producidos.
-    double nrmlz_numbers[NUMS_POR_SEC];  // Los números randoms normalizados.
+struct Sequence {  // Sequence struct
+    std::string dataID;  // dataID member, to identify sequence.
+    double mayor;  // Greatest number of sequence.
+    double media;  // Mean value of sequence.
+    double stdDev;  // Standard deviation for normalized sequence.
+    double clicks;  // Performance speed.
+    double rand_numbers[NUMS_POR_SEC];  // Randomly generated numbers.
+    double nrmlz_numbers[NUMS_POR_SEC];  // Normalized numbers.
 
-    // Métodos:
-
-    // Método para ordenar los números normalizados de menor a mayor usando el selection sort.
+    // Methods:
     void selectSort();
-    // Método para ordenar los números normalizados de menor a mayor usando el bubble sort.
     void bubbleSort();
 };
 
-/* Le pregunta al usuario cuántas secuencias se van a leer y devuelve ese número.
- Utiliza una variable 'num_seq', que guarda el número de secuencias a producir ingresado, y el mismo
-es devuelto. */
+/* Asks user how many sequences are to be read or created. Returns that value for other functions
+to use. */
 int userSequence();
 
-/* Le pregunta al usuario en dónde se van a guardar los archivos de las secuencias y si se va a leer
-un archivo para los números randoms o a crear uno nuevo. Toma como argumentos dos objetos fstream,
-'fileSeqs' y 'fileNomrs', que serán asociados a los archivos con las secuencias random y normalizadas
-respectivamente, un bool por referencia 'readFile' (que guarda si se va a leer un archivo de secuencias
-randoms o a crear uno), y el número de secuencias ingresado por el usuario. Utililza las variables
-'seqFilename' y 'normlicedFilename' para guardar los nombres de los archivos de las secuencias random
-y normalizadas respectivamente. */
+/* Asks user the name of files in which to save the randomly-generated sequences and their normalizations.
+Creates files with given names. Calls function 'readFile' that asks user whether to read from an existin
+file or create a new file with randomly-generated sequences. */
 void makeFilenames(fstream &, fstream &, bool &, int &);
 
-/* Crea las secuencias de números randoms en caso de no ser leídas. Toma como argumentos el vector
-'arrSecuencias' en donde se van a guardar las secuencias de tipo 'Sequence', el objeto fstream 'fileSeqs'
-asociado al archivo de las secuencias de núms. random, y el número de secuencias 'num_sec'. Utiliza las
-variables 'start', 'end', para comenzar a calcular los clicks, que son guardados dentro de la variable
-'clicks'. Utiliza la variable 'mayor' para guardar el número mayor por cada secuencia. */
+/* Creates randomly-generated sequences if instructed so. Saves each sequence in a sequences vector.
+Outputs sequences to fstream object file associated with the random sequences. Finds greatest number
+per sequence and stores it in sequence object member. */
 void makeSequences(fstream &, int, vector <Sequence> &);
 
-/* Lee las secuencias del archivo de secuencias de núms. random. Toma como argumentos dos objectos
-fstream 'fileSeqs' y 'fileNorms', asociados a los archivos de secuencias randoms y secuencias normalizadas,
-el núm. de secuencias 'num_secs' y el vector de secuencias 'arrSecuencias'. Utiliza variables como
-'menor', que se engarga de guardar el número menor entre todas las secuencias, la variable 'menor_pos',
-que guarda la posición (n, x) en donde se encuentra el número menor, donde n es el índice de la secuencia
-y x es la posición dentro de la misma secuencia. También contiene variables como 'mayor', que se utiliza para
-normalizar cada secuencia, en vez de tener que referirse al mismo como e.g. arrSecuencias[i].mayor por cada
-número dentro de cada secuencia. Contiene la variable 'deviation', que guarda la desviación por cada secuencia,
-la variable 'nrmlzCurrNum', que guarda el número normalizado actual, la variable 'media', que guarda la media
-de cada secuencia. */
+/* Reads sequences from random sequences file. Finds the smallest number between all sequences and saves
+its parent sequence's coordinates in the sequences array. Normalizes each sequences using the greatest number
+for the respective sequence and saves the sequences in an array. Also computes the standard deviation and
+mean value per sequence. */
 void getSequences(fstream &, fstream &, int, vector <Sequence> &);
 
-// Nuevas funciones implementadas:
-
-/* Calcula la desviación estándar de un arreglo de números normalizados. Toma como argumentos
-el arreglo de núms. normalizados 'randomSeq' y la media de los mismos 'seq_mean'. Devuelve la
-desviación estándar. Utiliza como variables 'std_deviation', que va acumulando el valor de
-desviación por cada número en la secuencia y finalmente es devuelto por la función. */
+/* Computes standard deviation given an array of numbers and their mean value. */
 double calcDeviation(double [], double);
 
-/* Recibe como argumento un string 'word' cualquiera y devuelve el mismo string en lowercase.
-Utiliza la variable 'lowered', que resulta ser un arreglo de caracteres, en donde se va a copiar
-los caracteres de la variable 'word', pero en lowercase. Este arreglo es entonces devuelto como
-string. */
+/* Returns parameter converted to lowercase. */
 string lowerCase(string);
 
-/* Lee las secuencias de un archivo de secuencias randoms existente en caso de que el mismo no se vaya
-a crear. Toma como argumentos el objeto fstream 'fileSeqs' asociado al archivo ya existente, el núm. de
-secuencias, el vector de secuencias 'arrSecuencias' (que va a ser repleto de secuencias a medida que se
-lea el archivo) y el número de secuencias 'num_sec'. Crea variables como 'id_text', que se encarga de leer
-el dataID de la secuencia, la variable 'random_number', que guarda el número random leído al momento, la
-variable 'clicks' que guarda el número de clicks de la secuencia, la variable 'mayor' que guarda el núm.
-mayor de la secuencia y la variable 'line_counter', que guarda el contedo de las líneas leídas mientras
-se lee el archivo. */
+/* Reads random sequences from an existing file and stores them in sequence objects. */
 void readSequences(fstream &, int &, vector <Sequence> &);
 
-/* Función que pregunta si el usuario desea leer un archivo de secuencias ya existence o crear uno nuevo.
-Valida hasta cierto punto la respuesta del usuario y finalmente devuelve su respuesta con un literal booleano;
-es decir, cierto si se va a leer de un archivo, o falso si no se va a leer. Utiliza la variable 'answer' para
-guardar la respuesta del usuario y validarla. */
+/* Asks if sequences are to be read or created and returns answer for other functions to use. */
 bool askReadFile();
 
-// Últimas funciones implementadas:
-
-/* Función que ordena el vector de secuencias utilizando el selection sort algorithm. Toma como argumentos
-el vector de secuencias 'arrSecuencias', el núm. de secuencias 'numOfSeqs' y el objeto fstream asociado al
-archivo de secuencias 'fileNorms' normalizadas. También desplega los clicks que se toma en ordenar el vector.
-Utiliza las variables 'start', 'end' que se encargan de medir los clicks, y la variable 'clicks' que guarda
-los clicks calculados. Utiliza la variable 'smallestMeanSeq', para guardar la secuencia con la media regresivamente
-más pequeña, y la variable 'sm_meanIdx' para guardar el índice de dicha secuencia, y lograr el swap del selection
-algorithm. */
+/* Uses Selection Sort algorithm to sort parameter array using the provided int parameter as sort key.
+Displays time taken to sort array. */
 void selectionSort(vector <Sequence>, int, fstream &);
 
-/* Función que ordena el vector de secuencias utilizando el bubble sort algorithm. Toma como argumentos
-el vector de secuencias 'arrSecuencias', el núm. de secuencias 'numsOfSeqs' y el objeto fstream "fileNorms"
-asociado al archivo de secuencias normalizadas. También desplega los clicks que se toma en ordenar el vector.
-Utiliza las variables 'start', 'end' y 'clicks' para medir y guardar los clicks, la variable 'swapped' para
-guardar si se ha hecho un cambio u ordenamiento en la secuencia. */
+/* Uses Bubble Sort algorithm to sort parameter array using the provided int parameter as sort key.
+Displays time taken to sort array. */
 void bubbleSort(vector <Sequence>, int, fstream &);
 
-/* Función que se encarga de desplegar (en el archivo) las secuencias del vector ya ordenado. 
-A su vez va ordenando las secuencias una por una, es decir; ordena los números en cada secuencia,
-y luego los va desplegando uno a uno en el archivo de secuencias normalizadas. También calcula el
-promedio que se tomó en ordenar cada secuencia y lo desplega (en la consola/terminal). Toma como
-argumentos el vector de secuencias 'arrSecuencias', el núm. de secuencias 'numsOfSeqs', el objeto
-fstream 'fileNorms' asociado al archivo de secuencias normalizadas, y un bool 'bubbleSort', que denota
-si se van a ordenar las secuencias con el bubble sort o con el selection sort. De esta manera,
-no habría necesidad de crear dos funciones aparte, para ordenar los números en las secuencias
-con el bubble y selection sort, sino que el parámetro bool determina cuál algoritmo de ordenamiento
-realizar, y lo demás aplica para ambos algoritmos. Utiliza las variables 'start', 'end' y 'clicks', para
-medir y guardar los clicks tomados en ordenar cada secuencia, y la variable 'sumOfClicks', para acumular
-todos los clicks y calcular su promedio. */
+/* Receives as parameter the ordered array from the previous two functions and outputs them to file
+object passed in, if not before calling the selection sort method or the bubble sort method on each
+sequence to sort them right before output. Display average time taken to sort each sequence internally. */
 void outSortedArray(vector <Sequence>, int, fstream &, bool =true);
 
 
--- main.cpp
+++ main.cpp
@@ -1,12 +1,9 @@
 /*
-    Carlos A. Hernández
-    CCOM 3033
-
-    Este programa se encarga de crear (o leer) un archivo de secuencias random. Cada
-    secuencia tendrá generalmente 10 números y la cantidad de secuencias es indicada
-    por el usuario. Luego de las secuencias random, se crea un nuevo archivo en donde
-    las secuencias van a ser normalizadas desplegadas, seguidas por la media y la desviación
-    estándar de la secuencia.
+    This program takes a file of randomly generated sequences and processess them, or
+    creates a new file of randomly generated sequences in order to process it. During
+    the processing phase this program will normalize the sequences, find the mean value,
+    find the standard deviation and sorts them using the Selection Sort and Bubble Sort
+    algorithms.
 */
 
 #include "Sequence.h"
@@ -15,34 +12,31 @@
 
 
 int main() {
-    srand((unsigned)time(0));  // Comienza una nueva semilla para los números randoms.
+    srand((unsigned)time(0));  // Start a new random seed using the current time.
 
-    int numberOfSeqs;  // Número de secuencias a crear (o leer).
-    bool readFile;  // 'true' si se va a leer un archivo existente.
-    fstream SeqsFile;  // fstream object para las secuencias random.
-    fstream NormlicedFile;  // fstream object para las secuencias normalizadas.
+    int numberOfSeqs;  // Number of sequences to be read or to be created.
+    bool readFile;  // Hold value of true if sequences are to be read.
+    fstream SeqsFile;  // fstream object to read random sequences file.
+    fstream NormlicedFile;  // fstream object wherein to output processed sequences.
 
-    /* Se le pregunta al usuario cúantas secuencias se van a leer (o a crear), y
-    se guarda en ésta variable. */
+    /*Ask user how many sequences are wanted to be processed and whether or not
+    to create said sequences on the spot or read them from an existing file. */
     numberOfSeqs = userSequence();
 
-    // Se crea un vector que guarda elementos de tipo 'Sequence', sin ningún elemento.
+    // Create a vector of sequences.
     vector <Sequence> Secuencias(0);
 
-    makeFilenames(SeqsFile, NormlicedFile, readFile, numberOfSeqs);  // Crea los archivos.
+    makeFilenames(SeqsFile, NormlicedFile, readFile, numberOfSeqs);  // Create files.
 
-    if (readFile)  // Si readFile es cierto; es decir, si se va a leer un archivo
-        // se leen los archivos.
+    if (readFile)  // If sequences are to be read, call readSequences function
         readSequences(SeqsFile, numberOfSeqs, Secuencias);
-    else
-        /* De lo contrario, crear un nuevo archivo y crear nuevas secuencias
-        / dentro del mismo. */
+    else  // Otherwise, create the sequences.
         makeSequences(SeqsFile, numberOfSeqs, Secuencias);
 
-    // Normalizar las secuencias.
+    // Normalize sequences.
     getSequences(SeqsFile, NormlicedFile, numberOfSeqs, Secuencias);
 
-    // Cerrar los archivos.
+    // Close files.
     SeqsFile.close();
     NormlicedFile.close();
     return 0;
